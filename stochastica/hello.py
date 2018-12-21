c1280d8f5ac1a0bbb0b778571cdd975c3acb885a
Dan Bolinson <danbolinson@gmail.com>
2018-12-21 14:29:22 -0800

    Added basic image template hello.html

--- stochastica/hello.py
+++ stochastica/hello.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return 'Hello world'
 
+    src = 'https://wallpaperbrowse.com/media/images/soap-bubble-1958650_960_720.jpg'
+    return render_template('hello.html', img=src)
+
+@app.route('/')
+def not404():
+    return "at least not a 404 error!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
