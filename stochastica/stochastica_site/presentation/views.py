8c56bd5bb75de20fe8457689b9ead5368d70f00c
ariannedee <ariannedee@gmail.com>
2019-02-07 11:37:44 -0800

    Properly set anonymous user

--- stochastica/stochastica_site/presentation/views.py
+++ stochastica/stochastica_site/presentation/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    if request.user == AnonymousUser:
+    if isinstance(request.user, AnonymousUser):
         return render(request, 'not_logged_in.html')
     image = request.user.subscribed_to.first().images.first()
     return render(request, 'index.html', context={'image': image.image.url})
