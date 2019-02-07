d6d6b5c24c4e66e91c26fbd605e7560a45155a20
ariannedee <ariannedee@gmail.com>
2019-02-07 11:08:01 -0800

    Simplify models, create migration, and add to admin

--- stochastica/stochastica_site/presentation/models.py
+++ stochastica/stochastica_site/presentation/models.py
@@ -1,43 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Image(models.Model):
-    image_url = models.ImageField(max_length=300) # limits URLs to 300 chars
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
-    deleted = models.BooleanField()
 
 class Pack(models.Model):
-    name = models.CharField('pack name', max_length=200) # optional field, for example, 'Starter Pack'
+    name = models.CharField(max_length=200)
     size = models.IntegerField(default=100)
+    subscribers = models.ManyToManyField(User, related_name='subscribed_to')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
-    deleted = models.BooleanField()
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted = models.BooleanField(default=False)
 
-class PackContents(models.Model):
-    pack = models.ForeignKey(Pack, on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, on_delete=models.PROTECT)
-        # This means each image can only be assigned to one pack, which I think is desired
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='pack_images', max_length=300)
+    pack = models.ForeignKey(Pack, related_name='images', on_delete=models.SET_NULL, blank=True, null=True)
+    viewed_by = models.ManyToManyField(User, related_name='images_viewed')
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
-    deleted = models.BooleanField()
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    deleted = models.BooleanField(default=False)
 
-class SubscribedTo(models.Model):
-    user = models.ManyToManyField(User)
-    pack = models.ManyToManyField(Pack)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField()
-    deleted = models.BooleanField()
-
-class ImageView(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    viewed_at = models.DateTimeField()
+    def __str__(self):
+        return self.title
 
 
 # Do we need to add attributes to the User class here? i.e. number of games played
