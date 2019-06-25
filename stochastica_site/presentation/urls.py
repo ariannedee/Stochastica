5d0a6c14e1620a23934041e956e15e13048f7278
danbolinson <dbolinson@gmail.com>
2019-06-24 22:27:14 -0400

    Added game_id to controller and to slide consumers so they communicate on the game_id channel.

--- stochastica_site/presentation/urls.py
+++ stochastica_site/presentation/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('play/<game_id>/', views.slide, name='slide'),
     path('next_round/<game_id>', views.next_round, name='next_round'),
-    path('play/next_round/<game_id>', views.next_round, name='next_round_2'), # Need to fix this, just a dumb js thing
+    path('play/<game_id>/next_round', views.next_round, name='next_round_2'), # Need to fix this, just a dumb js thing
     path('end_game/<game_id>', views.end_game, name='end_game'),
     path('how-to-play/', TemplateView.as_view(template_name='presentation/how-to-play.html'), name='instructions'),
-    path('play/controller/', views.controller, name='controller'),
+    path('controller/<game_id>', views.controller, name='controller'),
 ]
