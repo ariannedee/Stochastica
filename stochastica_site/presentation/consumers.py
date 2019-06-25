897dd25af7fca5af0b2ba1f921796a3311a31600
ariannedee <ariannedee@gmail.com>
2019-06-25 10:49:50 -0700

    Controllers control specific games with game_id

--- stochastica_site/presentation/consumers.py
+++ stochastica_site/presentation/consumers.py
@@ -1,12 +1,12 @@
-# chat/consumers.py
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
+
 class ControllerConsumer(WebsocketConsumer):
     def connect(self):
-        #self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'comm_%s' % 'controller'
+        self.room_name = self.scope['url_route']['kwargs']['game_id']
+        self.room_group_name = f'comm_{self.room_name}'
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -45,38 +45,3 @@ class ControllerConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': message
         }))
-
-
-class SlideConsumer(WebsocketConsumer):
-    def connect(self):
-        #self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'comm_%s' % 'controller'
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
